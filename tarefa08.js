304b186ace9c35731d2ac60450dbf8489ccacf1f
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-30 09:09:04 -0300

    Remove e subistitui o indice 3 do array

---
+++ tarefa08.js
@@ -0,0 +1,11 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador']
+
+// remove o indice e subistitui 3
+
+tarefas[3] = `dar banho no cachorro`
+console.table(tarefas)
+
+
+
+
+
