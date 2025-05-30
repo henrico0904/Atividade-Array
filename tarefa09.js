decd497f443ab3853f17c2c04134c7002489a836
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-30 09:14:30 -0300

    Concatena lista de tarefas

---
+++ tarefa09.js
@@ -0,0 +1,5 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador']
+let tarefas2 = [`Mandar mensagem pra ela`, `Cortar o cabelo`, `Limpar o vieogame`]
+
+//juntando as duas listas
+console.log(`Junção das listas: ${tarefas.concat(tarefas2).join(`, `)}`)
