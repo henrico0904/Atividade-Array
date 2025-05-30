5e798c4607ab8815b152f6b1b2262f6c864338f7
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-30 09:28:13 -0300

    Transforma Tarefas

--- tarefa10.js
+++ tarefa10.js
@@ -1,1 +1,5 @@
 let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador']
+// adiciona "fazer" na frente de cada elemento do array
+
+let palavraAdcionada = tarefas.map(item => "fazer: " + item)
+console.table(palavraAdcionada)
