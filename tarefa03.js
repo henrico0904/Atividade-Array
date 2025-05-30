5001a9ce617711f970d32785a3ab22f7942d9b57
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-30 08:32:37 -0300

    Adiciona mais uma tarefa ao início do array

---
+++ tarefa03.js
@@ -0,0 +1,4 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador']
+
+tarefas.unshift(`Levar o cachorro para passear`)
+console.log(`Mais uma tarefa: ${tarefas}`)
