6bee30daeb7515995b825285704d38bbb413cd13
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-30 08:36:35 -0300

    Remove a primeira tarefa do array

---
+++ tarefa04.js
@@ -0,0 +1,6 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador']
+//Remover a primeira tarefa do array
+
+let tarefaRemovida = tarefas.shift()
+console.log(`As tarefas são: ${tarefas}`)
+console.log(`A tarefa removida foi ${tarefaRemovida}`)
