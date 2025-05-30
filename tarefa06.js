76834d026ebd7ae585a35261a2641b5708e887f8
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-30 08:53:07 -0300

    transforma o array em strings e os separa com virgula

---
+++ tarefa06.js
@@ -0,0 +1,4 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador']
+//transforma o array em srings e separa por vírgula
+
+console.log(`tarefas separadas por vírgula: ${tarefas.join(`, `)}`)
