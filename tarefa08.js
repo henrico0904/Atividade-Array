f6e7acce99b3e9c26f33633aeb15dc24b17e8a50
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-30 09:35:39 -0300

    Altera a lista

--- tarefa08.js
+++ tarefa08.js
@@ -1,8 +1,11 @@
 let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador']
 
 // remove o indice e subistitui 3
+let tarefaRemovida = tarefas.splice(3, 1)
+let tarefaSubistituida = tarefas.splice(3, 0,"dar banho no cachorro")
 
-tarefas[3] = `dar banho no cachorro`
+console.log(`a tarefa removida foi ${tarefaRemovida}`)
+console.log(`a tarefa subistituida foi ${tarefaSubistituida}`)
 console.table(tarefas)
 
 
