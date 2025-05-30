336aa769adcb9b11637b647112baa1a4385515e5
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-30 09:36:44 -0300

    Organiza o array

---
+++ tarefa12.js
@@ -0,0 +1,4 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador']
+//organiza o array em ordem alfabética
+let ordenadas = tarefas.sort()
+console.log(ordenadas)
